e6a429c379192366ab3495ce17df7c4525e060cc
lucgbrl <lucassantos@alu.ufc.br>
2019-08-04 16:08:44 -0300

    Add main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+#!lucas/python/spell
+
+articles = {"a": '', 'de':'', 'por':'', 'e': ''}
+
+def __main__()
+	test_string = "Get @user comment"
+	print(test_string)
+	res = len(test_string.split()) #split words
+	res_mat = str(res) #count words 
+	return true
+
+def __prep_remove__(str)
+	str = test_string
+	str.split()
+	for i, j in str():
+		str_replace = str.replace(i, j)
+	endfor
+	return self.str	
+
+if name == "main"
+	__main__():
+	return self.main
